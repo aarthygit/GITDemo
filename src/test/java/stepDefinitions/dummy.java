033d07cf543eb5bd16bba5206fe851484622e245
aarthygit <aarthyarun2104@gmail.com>
2024-11-30 17:07:04 +0530

    this is commit from GITDEMO - asian time zone resource

---
+++ src/test/java/stepDefinitions/dummy.java
@@ -0,0 +1,15 @@
+package stepDefinitions;
+
+import org.testng.annotations.Test;
+
+public class dummy {
+	
+	@Test
+	public void postJira() {
+		System.out.println("postjira");
+		System.out.println("postjira");
+
+	}
+	
+
+}
