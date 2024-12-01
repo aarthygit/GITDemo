31e89fca2f7fe244c391668138c3132ea3809aa3
aarthygit <aarthyarun2104@gmail.com>
2024-12-01 13:51:35 +0530

    again commited by asian resource

--- src/test/java/stepDefinitions/dummy.java
+++ src/test/java/stepDefinitions/dummy.java
@@ -3,7 +3,7 @@ package stepDefinitions;
 import org.testng.annotations.Test;
 
 public class dummy {
-	
+
 	@Test
 	public void postJira() {
 		System.out.println("postjira");
@@ -13,10 +13,12 @@ public class dummy {
 		System.out.println("postjira2");
 
 		System.out.println("postjira3");
-		
-	
 
 	}
-	
+
+	public void postJira1() {
+		System.out.println("postjira");
+		System.out.println("postjira");
+	}
 
 }
