facbda25cc6f2569f63aa5f5eb05fa422fad12c7
aarthygit <aarthyarun2104@gmail.com>
2024-12-01 13:26:47 +0530

    american resource commit

--- src/test/java/stepDefinitions/dummy.java
+++ src/test/java/stepDefinitions/dummy.java
@@ -8,6 +8,14 @@ public class dummy {
 	public void postJira() {
 		System.out.println("postjira");
 		System.out.println("postjira");
+		System.out.println("postjira1");
+
+		System.out.println("postjira2");
+
+		System.out.println("postjira3");
+		
+	
+
 
 	}
 	
